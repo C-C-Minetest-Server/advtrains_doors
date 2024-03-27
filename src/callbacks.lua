e187a36144834eb8d80bcdfe855a83f843c92157
1F616EMO <root@1f616emo.xyz>
2024-03-27 11:29:46 +0800

    attempt-occ-cached

--- src/callbacks.lua
+++ src/callbacks.lua
@@ -23,33 +23,97 @@ local _ad = advtrains_doors
 local _int = _ad.internal
 local logger = _int.logger:sublogger("callbacks")
 
+local occ_reverse_lookup_sel = advtrains.occ.reverse_lookup_sel
+
+--[[
+    local def = minetest.registered_nodes[node.name]
+    if def._advtrains_doors_state == "closed" then
+        node.name = def._advtrains_doors_counterpart
+        minetest.swap_node(pos, node)
+    end
+
+    if def._advtrains_doors_state == "opened" then
+        node.name = def._advtrains_doors_counterpart
+        minetest.swap_node(pos, node)
+    end
+]]
+
+function _ad.set_door_state(pos, node, state)
+    local def = minetest.registered_nodes[node.name]
+    if (state == true and def._advtrains_doors_state == "closed")
+        or (state == false and def._advtrains_doors_state == "opened") then
+        node.name = def._advtrains_doors_counterpart
+        minetest.swap_node(pos, node)
+    end
+end
+
+-- {[hash] = {state, last_updated}, ...}
+local check_cache = {}
+
 minetest.register_abm({
     label = "Open/Close advtrains doors",
-    nodenames = {"group:advtrains_doors"},
+    nodenames = { "group:advtrains_doors" },
     interval = 0.1,
     chance = 1,
     catch_up = false,
     action = function(pos, node, active_object_count, active_object_count_wider)
-        local def = minetest.registered_nodes[node.name]
-        local dir = vector.multiply(minetest.fourdir_to_dir(node.param2), -1) -- trains are in front of the gate
-        for _, entity in ipairs(minetest.get_objects_inside_radius(vector.add(pos, dir), 2)) do
-            local luaentity = entity:get_luaentity()
-            if luaentity and luaentity.is_wagon then
-                local train = luaentity:train()
-                if train.velocity == 0 and train.door_open ~= 0 then
-                    -- the door is opened, open this gate
-                    if def._advtrains_doors_state == "closed" then
-                        node.name = def._advtrains_doors_counterpart
-                        minetest.swap_node(pos, node)
+        local hash = minetest.hash_node_position(pos)
+        if check_cache[hash] then
+            if os.clock() - check_cache[hash][2] <= 0.1 then
+                _ad.set_door_state(pos, node, check_cache[hash][1])
+                return
+            end
+            check_cache[hash] = nil
+        end
+
+        local dir = minetest.fourdir_to_dir(node.param2)
+        local cpos = pos + (dir * -1) + vector.new(0, -1, 0)
+        local trains, _ = occ_reverse_lookup_sel(cpos, "in_train")
+        for train_id, _ in pairs(trains) do
+            local train = advtrains.trains[train_id]
+
+            local doorstate = false
+            if train.velocity == 0 and train.door_open ~= 0 then
+                doorstate = true
+            end
+            local now = os.clock()
+
+            for i, w_id in ipairs(train.trainparts) do
+                local data = advtrains.wagons[w_id]
+                local _, wagon = advtrains.get_wagon_prototype(data)
+
+                -- Get the train direction
+                -- we use the old position logic cuz that should be enough
+                -- see advtrains/advtrains/wagons.lua:424-452
+                local index = advtrains.path_get_index_by_offset(train, train.index, -data.pos_in_train)
+                local tpos, yaw, npos, npos2 = advtrains.path_get_interpolated(train, index)
+                local vdir = vector.normalize(vector.subtract(npos2, npos))
+
+                -- Get the two endpoints of the train
+                local minp = vector.round(tpos + (vdir * -wagon.wagon_span))
+                local maxp = vector.round(tpos + (vdir * wagon.wagon_span))
+                minp, maxp = vector.sort(minp, maxp)
+
+                for x = minp.x, maxp.x do
+                    for y = minp.y, maxp.y do
+                        for z = minp.z, maxp.z do
+                            local check_pos = vector.new(x, y + 1, z) + dir
+                            local check_hash = minetest.hash_node_position(check_pos)
+                            if doorstate then
+                                check_cache[check_hash] = { true, now }
+                            else
+                                if not (check_cache[check_hash] and check_cache[check_hash][1] == true) then
+                                    -- Do not override open doors
+                                    check_cache[check_hash] = { false, now }
+                                end
+                            end
+                            if check_hash == hash then
+                                _ad.set_door_state(pos, node, check_cache[check_hash][1])
+                            end
+                        end
                     end
-                    return
                 end
             end
-        end
-        -- no wagon with opened door found, close this gate
-        if def._advtrains_doors_state == "opened" then
-            node.name = def._advtrains_doors_counterpart
-            minetest.swap_node(pos, node)
         end
     end,
 })
